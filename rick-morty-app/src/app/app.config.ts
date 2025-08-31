64f6af5060c9e4ad3af95d1ce2097ab3e531a726
Jennifer Arabel <jennifer.arabel@gmail.com>
2025-08-31 10:30:05 -0300

    Add HashLocationStrategy to app configuration for improved routing to deploy app in Netlify

--- rick-morty-app/src/app/app.config.ts
+++ rick-morty-app/src/app/app.config.ts
@@ -3,12 +3,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(), // Global error handling
     provideZoneChangeDetection({ eventCoalescing: true }), // Optimized change detection
     provideRouter(routes), // Router configuration
-    provideHttpClient() // HTTP client for API calls
-  ]
+    provideHttpClient(), // HTTP client for API calls
+
+    //HashStrategy
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    },
+  ],
 };
