a3edc3e1449ad583128628f7c2e571ca852a2c13
Jennifer Arabel <jennifer.arabel@gmail.com>
2025-08-31 09:14:13 -0300

    Remove unused import of CharacterEpisodesComponent from CharacterInfoComponent

--- rick-morty-app/src/app/components/character-info/character-info.component.ts
+++ rick-morty-app/src/app/components/character-info/character-info.component.ts
@@ -1,12 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { CharacterEpisodesComponent } from '../character-episodes/character-episodes.component';
 import { Character } from '../../interfaces/ApiResponse';
 
 @Component({
   selector: 'character-info',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage, CharacterEpisodesComponent],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './character-info.component.html',
 })
 export class CharacterInfoComponent {
