b1fc08ff5c3d88b4c7dc63a08ce5458a330360b7
Jennifer Arabel <jennifer.arabel@gmail.com>
2025-08-31 09:01:30 -0300

    Add NotFoundComponent and update routing for 404 errors

--- rick-morty-app/src/app/app.routes.ts
+++ rick-morty-app/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CharactersPageComponent } from './pages/characters-page/characters-page.component';
 import { CharacterDetailsComponent } from './pages/character-details/character-details.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -17,7 +18,11 @@ export const routes: Routes = [
     pathMatch: 'full'
   },
   {
+    path: '404',
+    component: NotFoundComponent
+  },
+  {
     path: '**',
-    redirectTo: 'characters',
+    redirectTo: '404',
   },
 ];
