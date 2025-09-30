5628c64deb71688b713295d73331e6d03b7fc64d
Jennifer Arabel <jennifer.arabel@gmail.com>
2025-09-30 19:40:15 -0300

    Crear User Interface

---
+++ rick-morty-app/src/app/interfaces/User.ts
@@ -0,0 +1,12 @@
+export interface User {
+  email: string;
+  name: string;
+  address?: string;
+  city?: string;
+  state?: string;
+  zip?: string;
+  phone?: string;
+  birthday?: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
