58bbd8542b29d9c259546eb12551f1362835a073
Jennifer Arabel <jennifer.arabel@gmail.com>
2025-10-01 06:27:05 -0300

    Implementar guard de autenticación y mejorar validaciones de formularios

--- rick-morty-app/src/app/app.routes.ts
+++ rick-morty-app/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { CharactersPageComponent } from './pages/characters-page/characters-page.component';
 import { CharacterDetailsComponent } from './pages/character-details/character-details.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { authGuard } from './auth/guards/auth.guard';
 
 export const routes: Routes = [
   {
@@ -11,22 +12,24 @@ export const routes: Routes = [
   {
     path: 'characters',
     component: CharactersPageComponent,
+    canActivate: [authGuard],
   },
   {
     path: 'characters/:id',
     component: CharacterDetailsComponent,
+    canActivate: [authGuard],
   },
-  // {
-  //   path: '',
-  //   redirectTo: 'auth/login',
-  //   pathMatch: 'full'
-  // },
-  // {
-  //   path: '404',
-  //   component: NotFoundComponent
-  // },
+  {
+    path: '',
+    redirectTo: 'auth/login',
+    pathMatch: 'full'
+  },
+  {
+    path: '404',
+    component: NotFoundComponent
+  },
   {
     path: '**',
-    redirectTo: 'auth',
+    redirectTo: 'auth/login',
   },
 ];
