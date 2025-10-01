611121d860d40ec53af1ee827e5f10f764f9d462
Jennifer Arabel <jennifer.arabel@gmail.com>
2025-10-01 07:14:10 -0300

    Agregar guard de autenticación a las rutas de login y registro para evitar acceso a usuarios autenticados

--- rick-morty-app/src/app/auth/auth.routes.ts
+++ rick-morty-app/src/app/auth/auth.routes.ts
@@ -1,15 +1,18 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import {  noAuthGuard } from './guards/no-auth.guard';
 
 export const authRoutes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+    canActivate: [noAuthGuard], // Evita que usuarios autenticados accedan al login
   },
   {
     path: 'register',
     component: RegisterComponent,
+    canActivate: [noAuthGuard], // Evita que usuarios autenticados accedan al register
   },
   {
     path: '**',
